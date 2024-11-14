1d9f751a8ecae20f0d9485474836a3d8ff9985c5
MYKOLL DANIEL CUNHA GUIMARAES <mykoll.dcguimaraes@senacsp.edu.br>
2024-11-13 21:23:19 -0300

    primeiro envio

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
+import Onboard from './pages/Onboard';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -35,19 +36,32 @@ import './theme/variables.css';
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  return (
+    <IonApp>
+
+      <IonReactRouter>        
+        <IonRouterOutlet>
+          
+          <Route exact path="/onboard">
+            <Onboard />
+          </Route>
+
+          <Route exact path="/home">
+            <Home />
+          </Route>
+
+          <Route exact path="/">
+            <Redirect to="/onboard" />
+          </Route>
+
+        </IonRouterOutlet>
+
+      </IonReactRouter>
+
+
+    </IonApp>
+  )
+};
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,22 +2,34 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/rea
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
-const Home: React.FC = () => {
+const Home = () => {
   return (
     <IonPage>
+
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>ok</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen>
+
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+
+        <div id="container">
+          <strong>Ready to create an app?</strong>
+          <p>Start with Ionic 
+            <a target="_blank" rel="noopener noreferrer" 
+            href="https://ionicframework.com/docs/components">UI Components</a>
+          </p>
+        </div>
+
       </IonContent>
+
     </IonPage>
   );
 };
